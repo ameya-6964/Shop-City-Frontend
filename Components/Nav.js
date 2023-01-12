d700ea778ecb204f71e40bdce12fb8db440963fe
ameya-6964 <belvalkar7@gmail.com>
2023-01-12 17:55:14 +0530

    Added Toggle To Cart

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,17 +1,22 @@
 import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
-import { NavItems, NavStyles } from "../styles/NavStyles";
+import { NavStyles, NavItems } from "../styles/NavStyles";
+import Cart from "./Cart";
+import { useStateContext } from "../lib/context";
 
 export default function Nav() {
+  const { showCart, setShowCart } = useStateContext();
+  console.log(showCart);
   return (
     <NavStyles>
-      <Link href="/">Shop City.</Link>
+      <Link href="/">Styled.</Link>
       <NavItems>
-        <div>
+        <div onClick={() => setShowCart(true)}>
           <FiShoppingBag />
           <h3>Cart</h3>
         </div>
       </NavItems>
+      {showCart && <Cart />}
     </NavStyles>
   );
 }
