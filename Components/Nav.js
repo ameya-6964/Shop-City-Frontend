9d052d3a2d3d05a991b9cdf08e6d08bc739647f5
ameya-6964 <belvalkar7@gmail.com>
2023-01-12 17:34:27 +0530

    Added NavItems Components

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
-import { NavStyles } from "../styles/NavStyles";
+import { NavItems, NavStyles } from "../styles/NavStyles";
 
 export default function Nav() {
   return (
     <NavStyles>
       <Link href="/">Shop City.</Link>
-      <div>
+      <NavItems>
         <div>
           <FiShoppingBag />
           <h3>Cart</h3>
         </div>
-      </div>
+      </NavItems>
     </NavStyles>
   );
 }
